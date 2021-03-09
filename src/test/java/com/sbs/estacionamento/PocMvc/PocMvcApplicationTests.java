7d47d5df056490e9130e20b57826fbff59df3ee9
Sergio Bezerra <sergio.bezerra@outlook.com>
2021-03-09 14:51:02 -0300

    first commit

---
+++ src/test/java/com/sbs/estacionamento/PocMvc/PocMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.estacionamento.PocMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
