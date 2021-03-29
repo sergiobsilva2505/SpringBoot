0d23d54770f26f6345c833cbf43bb9bcd636e468
Sergio Bezerra <sergio.bezerra@outlook.com>
2021-03-28 21:24:58 -0300

    test: veiculoService, findAll

---
+++ src/test/java/com/sbs/estacionamento/pocmvc/service/VeiculoServiceTest.java
@@ -0,0 +1,30 @@
+package com.sbs.estacionamento.pocmvc.service;
+
+import com.sbs.estacionamento.pocmvc.PocMvcApplicationTests;
+import com.sbs.estacionamento.pocmvc.entities.Veiculo;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VeiculoServiceTest extends PocMvcApplicationTests {
+
+    @Autowired
+    private VeiculoService veiculoService;
+
+    @Test
+    public  void deveRetornarUmaListaDeVeiculosTest(){
+        List<Veiculo> veiculos = veiculoService.findAll();
+        int countVeiculo = veiculoService.findAll().size();
+        Assert.assertEquals(countVeiculo, veiculos.size());
+    }
+
+}
